c357752918410f0b9f58d31b81ebe32f949322b8
Adarsh <adarsh.sn@coviam.com>
2018-06-26 12:40:45 +0530

    application properties changes

--- ContestAPI/src/main/java/com/constest/ContestAPI/controller/UserAnswerController.java
+++ ContestAPI/src/main/java/com/constest/ContestAPI/controller/UserAnswerController.java
@@ -1,52 +1,50 @@
 package com.constest.ContestAPI.controller;
 
 import com.constest.ContestAPI.dto.UserAnswerDTO;
-import com.constest.ContestAPI.entity.ContestEntity;
 import com.constest.ContestAPI.entity.UserAnswerEntity;
-import com.constest.ContestAPI.service.UserPointsService;
+import com.constest.ContestAPI.service.UserAnswerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/contest/")
 public class UserAnswerController {
 
     @Autowired
-    UserPointsService userPointsService;
+    UserAnswerService userAnswerService;
 
-    @RequestMapping(method = RequestMethod.POST, value = "save")
+    @RequestMapping(method = RequestMethod.POST, value = "saveAnswer")
     public Boolean userAnswer(@RequestBody UserAnswerDTO userAnswerDTO) {
-        Boolean userAnswerDTO1 = userPointsService.save(userAnswerDTO);
+        Boolean userAnswerDTO1 = userAnswerService.save(userAnswerDTO);
         return userAnswerDTO1;
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "getAnswer/{userAnswerId}")
     public String getAnswer(@PathVariable String userAnswerId) {
-        return (userPointsService.getAnswer(userAnswerId));
+        return (userAnswerService.getAnswer(userAnswerId));
     }
 
 
     @RequestMapping(method = RequestMethod.GET, value = "getAllAnswers/{userId}")
     public List<UserAnswerEntity> getAllAnswers(@PathVariable String userId) {
-        return userPointsService.getUserAllAnswers(userId);
+        return userAnswerService.getUserAllAnswers(userId);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "getUserPoints/{userId}")
     public Integer getUserPoints(@PathVariable String userId) {
-        return userPointsService.getUserScore(userId);
+        return userAnswerService.getUserScore(userId);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "getFastestUser/{contestQuestionId}")
     public String getFastestUser(@PathVariable String contestQuestionId) {
-        return userPointsService.getFastestAnswer(contestQuestionId);
+        return userAnswerService.getFastestAnswer(contestQuestionId);
     }
 
     // @RequestMapping(method = RequestMethod.GET, value = "getUserContestPoints/{contestId}/{userId}")
     // public Integer getUserContestPoints(@PathVariable String contestId, @PathVariable String userId) {
-    //     return userPointsService.getUserContestPoints(contestId, userId);
+    //     return userAnswerService.getUserContestPoints(contestId, userId);
     // }
 
 
