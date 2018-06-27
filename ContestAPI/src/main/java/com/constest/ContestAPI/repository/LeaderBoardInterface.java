7711289c4fb0d53a5f4c8e31a9d70212967e2b5f
Chalana D A <chalanada@Chalanas-MacBook-Pro.local>
2018-06-27 17:34:32 +0530

    updated leaderBoard using custom query

--- ContestAPI/src/main/java/com/constest/ContestAPI/repository/LeaderBoardInterface.java
+++ ContestAPI/src/main/java/com/constest/ContestAPI/repository/LeaderBoardInterface.java
@@ -1,6 +1,5 @@
 package com.constest.ContestAPI.repository;
 
-import com.constest.ContestAPI.entity.OverAllLeaderBoardEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
--- ContestAPI/src/main/java/com/constest/ContestAPI/repository/UserPointsRepository.java
+++ ContestAPI/src/main/java/com/constest/ContestAPI/repository/UserPointsRepository.java
@@ -1,7 +1,9 @@
 package com.constest.ContestAPI.repository;
 
 import com.constest.ContestAPI.entity.ContestEntity;
+import com.constest.ContestAPI.entity.LeaderBoard;
 import com.constest.ContestAPI.entity.UserPointsEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,5 +14,18 @@ public interface UserPointsRepository extends CrudRepository<UserPointsEntity,St
 
     List<UserPointsEntity> findAllByUserId(String userId);
     List<UserPointsEntity> findAllByContestEntity(ContestEntity contestEntity);
+
+    @Query(value = "SELECT * FROM user_points p where p.contest_id = ?#{[0]} order by final_points desc",nativeQuery = true)
+    List<UserPointsEntity> test(String contest_id);
+
+
+//    @Query("SELECT new com.constest.ContestAPI.entity.LeaderBoard(v.userId,v.finalPoints) FROM UserPointsEntity v")
+//    List<LeaderBoard> findSurveyCount();
+
+    @Query(value ="SELECT v.user_id,v.final_points,dense_rank() over(order by v.final_points desc) FROM user_points v where v.contest_id = ?#{[0]}",nativeQuery = true)
+    List<Object[]> getContestWiseLeaderBoard(String contest_id);
+
+    @Query(value = "SELECT v.user_id,sum(v.final_points)as over_all_points,dense_rank() over (order by sum(v.final_points) desc) FROM user_points v group by v.user_id",nativeQuery = true)
+    List<Object[]> overAllBoard();
 
 }
